8d28e0ab0455f98e651ad730573bd49f494747e3
Henri Susanto <henry.dinus@gmail.com>
2024-10-05 09:24:38 +0700

    init

---
+++ filterable-gallery.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ *  Plugin Name: Filterable Gallery
+ * 	Plugin URI: http://builderla.com/
+ * 	Description: Wordpress plugin for filterable gallery
+ * 	Version: 1.0
+ * 	Author: Javier Basso
+ * 	Author URI: http://www.doctormopar.com
+ */
